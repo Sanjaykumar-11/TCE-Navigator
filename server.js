31658b9b54fc294c0e26066a212f28aef7825d61
Sanjaykumar S <63301273+Sanjaykumar-11@users.noreply.github.com>
2021-12-17 18:29:36 +0530

    temp

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//Initialization
 const express = require('express');
 const sql = require('mysql2');
 const bp = require('body-parser');
@@ -8,6 +9,7 @@ const app = express();
 const path = require('path');
 const { Console } = require('console');
 
+//app uses
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: false }));
 app.use(express.static('images'));
@@ -20,11 +22,12 @@ app.use(session({
     saveUninitialized: false
 }));
 
+//database configuration
 const db = sql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'spmp',
+    database: 'tce_navigator',
     port: '3306'
 });
 
@@ -39,6 +42,7 @@ app.listen(3100, () => {
     console.log("Server listening to port 3100!!!!")
 })
 
+//get funtions
 app.get('/', (req, res)=>{
     res.sendFile(`${__dirname}/index.html`);
 })
@@ -47,3 +51,78 @@ app.get('/userLogin', (req, res)=>{
     res.sendFile(`${__dirname}/login.html`);
 })
 
+app.get('/addevents', (req, res)=>{
+    if(req.session.loggedin = true)
+    {
+        res.sendFile(`${__dirname}/addevents.html`);
+    }
+    else
+    {
+        res.sendFile(`${__dirname}/login.html`);
+    }
+    
+})
+
+app.get('/register', (req, res)=>{
+    res.sendFile(`${__dirname}/register.html`)
+})
+
+
+//post functions
+app.post('/user', (req, res)=>{
+    req.session.loggedin = false;
+    var email = req.body.email;
+    console.log(email)
+    
+    var password = req.body.password;
+    console.log(password)
+
+    if (email && password) {
+        db.query(`SELECT * FROM user WHERE mail = '${email}' `, function(error, results) {
+            if(error) throw error;
+            if (results.length > 0) {
+                var hash = results[0].password;
+
+                const passwordHash = bcrypt.hashSync(password, 10);
+                console.log(passwordHash)
+                const verified = bcrypt.compareSync(password, hash);
+
+                if (verified) {
+                    req.session.loggedin = true;
+                    req.session.email = email;
+                    req.session.name = results[0].name;
+                    res.redirect('/addevents');
+                } else {
+                    res.write(`<script>window.alert('Enter the correct password!!!!!');window.location.href = '/userLogin';</script>`);
+                }
+
+            } else {
+
+                res.write(`<script>window.alert('Enter the correct email!!!!!');window.location.href = '/userLogin';</script>`)
+            }
+            res.end();
+        });
+    } else {
+        res.write(`<script>window.alert('Enter  password and email!!!!!!');window.location.href = '/userLogin';</script>`)
+    }
+})
+
+app.post('/register', (req, res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password1 = req.body.password1;
+    if(password === password1)
+    {
+        const passwordHash = bcrypt.hashSync(password, 10);
+        let qr = `INSERT INTO user(name, mail, password) VALUES('${name}', '${email}', '${passwordHash}')`
+        db.query(qr, (error, result)=>{
+            if(error) throw error;
+            res.write(`<script>window.alert('Registration successful');window.location.href = '/userLogin';</script>`)
+        })
+    }
+    else
+    {
+        res.write(`<script>window.alert('Password doesn't match!');window.location.href = '/register';</script>`)
+    }
+})
